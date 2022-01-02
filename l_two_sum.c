ffa3c93e5450bcee2f2a6de1280a22aea0544d8a
JEON EUNYUB <79236945+jeoney@users.noreply.github.com>
2022-01-02 18:19:02 +0900

    Update l_two_sum.c
    using binary search O(NlogN)

--- l_two_sum.c
+++ l_two_sum.c
@@ -7,8 +7,109 @@ int num_len;
 int nums[MAXN];
 int output[2];
 int target;
-int dbg=1;
+int dbg=0;
 
+typedef struct {
+	int idx;
+	int val;
+}INPUT;
+INPUT input[MAXN];
+
+	// O(NlogN) 
+	// 입력 structure idx/value
+	// sort
+	// loop 돌면서 binary search로 찾기
+	
+int compare (const void * a, const void * b)
+{
+	INPUT first = *(INPUT *)a;
+	INPUT second = *(INPUT *)b;
+	if ( first.val < second.val ) return -1;
+	else return 1;
+}
+
+int bin_srch(int s, int e, int match_num)
+{
+	int result = -1;
+	int m = 0;
+	
+	while(s<e)
+	{
+		m = (s+e)/2;
+		if(input[m].val == match_num)
+		{
+			if(dbg) printf("found matched number val:%d, idx: %d\n", input[m].val, input[m].idx);
+			return input[m].idx;
+		}
+		else if(input[m].val > match_num)
+		{
+			e = m;
+		}
+		else
+		{
+			s = m;
+		}
+	}
+
+	return result;
+}
+
+void find_two_num(int target, int num_len, int *ret1, int *ret2)
+{
+	int i=0, result=0;
+	//앞에서 부터 loop 돌면서 찾아야 할 match num 확보
+	for(i=0; i<num_len-1; i++) //check before num_len
+	{
+		//전체 INPUT에서 match num있는지 이분탐색
+		result = bin_srch(i, num_len, target-input[i].val);
+		
+		//찾으면 return 아니면 다음 index 검색 if result == -1, not found
+		if(result != -1)
+		{
+			*ret1 = input[i].idx;
+			*ret2 = result;
+			return;
+		}
+	}
+
+	return;
+}
+
+int main()
+{
+	int i, j = 0;
+	int ret1, ret2 = 0;
+	scanf("%d", &target);
+	scanf("%d", &num_len);
+	for(i=0; i<num_len; i++)
+	{
+		input[i].idx = i;
+		scanf("%d", &input[i].val);
+	}
+	
+	if(dbg)
+	{
+		for(i=0; i<num_len; i++)
+			printf("idx:%d, val:%d\n", input[i].idx, input[i].val);
+	}
+
+	//정렬
+	qsort (input, num_len, sizeof(INPUT), compare);
+
+	if(dbg)
+	{
+		for(i=0; i<num_len; i++)
+			printf("idx:%d, val:%d\n", input[i].idx, input[i].val);
+	}
+
+	find_two_num(target, num_len, &ret1, &ret2);
+	
+	printf("%d %d\n", ret1, ret2);
+
+	return 0;
+}
+
+/*
 int main()
 {
 	int i, j = 0;
@@ -38,11 +139,11 @@ int main()
 	// loop 돌면서 binary search로 찾기
 	
 	
-	//Hash로 찾기 => 이건 C++로 해야할듯...
+	//Hash로 찾기  
 	
 	return 0;
 }
-
+*/
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
